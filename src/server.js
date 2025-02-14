cec184ca32a079b7209194d710905028b329f0ed
Phuong Nam <thaidagpnam@gmail.com>
2025-02-14 14:15:19 +0700

    done video #23

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const path = require('path');
+require('dotenv').config();
 
-const app = express()
-const port = 8081
+const app = express();
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 // config template engine
 app.set('views', path.join(__dirname, 'views')); // chỉ định folder chứa các view cần thiết, không sử dụng đường dẫn tương đối để tránh sai đường dẫn -> tuyệt đối
 app.set('view engine', 'ejs')
+
+// config static files
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -22,6 +27,6 @@ app.get('/iFanIT', (req, res) => {
   res.render('sample.ejs');
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
