801567f713b73aefa4b31568b4833d9f08b6e9cb
Phuong Nam <thaidagpnam@gmail.com>
2025-03-09 16:49:07 +0700

    nothing

--- src/config/database.js
+++ src/config/database.js
@@ -1,24 +1,35 @@
 require('dotenv').config();
-const mysql = require('mysql2/promise');
+const mongoose = require('mongoose');
 
-// Create the connection to database
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT, // điều này là cần thiết bởi vì nếu ta không chỉ định thì nó sẽ chạy mặc định trên cổng 3306 đã có bên docker -> lỗi
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD, // nếu không chỉ định password thì nó sẽ default password là rỗng (đăng nhập ko cần password)
-//     database: process.env.DB_NAME
-// });
 
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // điều này là cần thiết bởi vì nếu ta không chỉ định thì nó sẽ chạy mặc định trên cổng 3306 đã có bên docker -> lỗi
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD, // nếu không chỉ định password thì nó sẽ default password là rỗng (đăng nhập ko cần password)
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+const dbState = [{
+  value: 0,
+  label: "disconnected"
+},
+{
+  value: 1,
+  label: "connected"
+},
+{
+  value: 2,
+  label: "connecting"
+},
+{
+  value: 3,
+  label: "disconnecting"
+}];
+
+
+const connection = async () => {
+  const options = {
+    user: "process.env.DB_USER",
+    pass: process.env.DB_PASSWORD
+  }
+
+  await mongoose.connect(process.env.DB_HOST, options);
+  const state = Number(mongoose.connection.readyState);
+  console.log(dbState.find(f => f.value == state).label, "to db"); // connected to db
+
+}
 
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const express = require('express')
-const configViewEngine = require('./config/viewEngine');
-const webRoutes = require('./routes/web');
+require("dotenv").config();
+const express = require("express");
+const configViewEngine = require("./config/viewEngine");
+const webRoutes = require("./routes/web");
 
-const connection = require('./config/database');
+const connection = require("./config/database");
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -17,13 +17,19 @@ app.use(express.urlencoded({ extended: true })); // khi bên client gửi đi th
 configViewEngine(app);
 
 // khai báo route
-app.use('/', webRoutes);
+app.use("/", webRoutes);
 
-// test connection 
+// đây là cách sử dụng arrow function không cần đặt tên trước - self-running function - giúp chúng ta không cần gọi
+// hàm 1 cách thủ công nữa
+(() => {
+  // test connection
+  try {
+    connection();
+    app.listen(port, hostname, () => {
+      console.log(`My Apple Project app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(">>> Error connect to DB: ", error);
+  }
+})();
 
-// Query 
-
-
-app.listen(port, hostname, () => {
-  console.log(`Example app listening on port ${port}`)
-})
