700647036d4ff0f470fadbe1024eef218ff89927
Phuong Nam <thaidagpnam@gmail.com>
2025-03-10 08:16:23 +0700

    done video #59 - kết nối thành công và tạo một collection dưới MongoDB

--- src/config/database.js
+++ src/config/database.js
@@ -22,14 +22,14 @@ const dbState = [{
 
 const connection = async () => {
   const options = {
-    user: "process.env.DB_USER",
-    pass: process.env.DB_PASSWORD
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME
   }
 
   await mongoose.connect(process.env.DB_HOST, options);
-  const state = Number(mongoose.connection.readyState);
+  const state = Number(mongoose.connection.readyState); // mongoose.connection.readyState ở đây trả về kiểu số rồi, tương ứng với các số 0, 1, 2, 3 như trên để cho biết trạng thái hiện tại của kến nối đến db MongoDB
   console.log(dbState.find(f => f.value == state).label, "to db"); // connected to db
-
 }
 
 module.exports = connection;
