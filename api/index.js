cc9b5d71cfe303134d8730b30483adf466650814
Phuong Nam <thaidagpnam@gmail.com>
2025-04-04 10:19:53 +0700

    deploy vercel cấu hình thêm folder api

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
-const app = require("../src/server");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
